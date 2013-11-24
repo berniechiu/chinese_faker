34d3960214b25e031fe006e00a7bd96a29c1156e
Bernie Chiu <bernie_chiu@hotmail.com>
2013-11-24 14:13:09 +0800

    Support i18n locale structure

--- lib/chinese_faker.rb
+++ lib/chinese_faker.rb
@@ -1,3 +1,38 @@
 # encoding: utf-8
-require 'chinese_faker/name'
-require 'chinese_faker/job'
+mydir = File.expand_path(File.dirname(__FILE__))
+
+require 'i18n'
+I18n.load_path += Dir[File.join(mydir, 'locales', '*.yml')]
+I18n.reload!
+I18n.default_locale = "zh-tw"
+
+module ChineseFaker
+
+  class Base
+    class << self
+      def parse(key, with_title=false)
+        parsed = []
+
+        I18n.translate("chinese_faker.#{key}").each do |format|
+          name = format.split
+          parsed << name.map do |str|
+            fetch("name.#{str[str.index('{') + 1, str.index('}') - 2]}")
+          end.join
+
+        end
+
+        return with_title ? parsed[0] : parsed[1]
+      end
+
+      def fetch(key)
+        fetched = I18n.translate("chinese_faker.#{key}")
+        items = fetched[0].split
+
+        return items[rand(items.length)]
+      end
+    end
+  end
+end
+
+require './chinese_faker/name'
+require './chinese_faker/job'
--- lib/chinese_faker/name.rb
+++ lib/chinese_faker/name.rb
@@ -1,16 +1,15 @@
 # encoding: utf-8
 module ChineseFaker
 
-  class Name
-    LAST_NAMES = %w[王 李 張 劉 陳 楊 黃 趙 吳 周 邱 廖 郭 陳 蔡 江 何 蕭 羅 呂 高 彭 朱 
-                    詹 胡 簡 沈 施 柯 盧 余 翁 潘 游 魏 顏 方 宋 侯 馮 童]
-    FIRST_NAMES = %w[志豪 至明 志偉 冠宇 冠廷 偉 敏 秀英 芳]
+  class Name < Base
 
     class << self
-      def name
-        return LAST_NAMES[rand(LAST_NAMES.length)] +
-               FIRST_NAMES[rand(FIRST_NAMES.length)]
+      def name(opt = { with_title: false })
+        parse('name.name', opt[:with_title])
       end
+
+      def first_name; fetch('name.first_name'); end
+      def last_name; fetch('name.last_name'); end
     end
   end
 end
